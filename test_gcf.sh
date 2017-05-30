330452a0da96ee36477d31ba4a21e96775617e53
Reiko HISADA <january108.gmail.com>
2017-05-30 19:53:11 +0900

    add calc Greatest common factor script
    add test_target and test

---
+++ test_gcf.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+tmp=/tmp/$$
+
+ERROR_EXIT () { 
+  echo "$1" >&2
+  rm -f $tmp
+  exit 1
+}
+
+#########
+# test1 #
+#########
+echo 3 > $tmp-ans
+./gcf.sh 15 21 > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+echo test1 OK
+
+#########
+# test2 #
+#########
+echo 1 > $tmp-ans
+./gcf.sh 10039 2 > $tmp-out || ERROR_EXIT "TEST2-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST2-2"
+echo test2 OK
+
+#########
+# test3 #
+#########
+echo "自然数を２つ指定してください" > $tmp-ans
+./gcf.sh  > $tmp-out && ERROR_EXIT "TEST3-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST3-2"
+echo test3 OK
+
+#########
+# test4 #
+#########
+echo "自然数を２つ指定してください" > $tmp-ans
+./gcf.sh 10 > $tmp-out && ERROR_EXIT "TEST4-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST4-2"
+echo test4 OK
+
+#########
+# test5 #
+#########
+echo "自然数を指定してください" > $tmp-ans
+./gcf.sh 100.139 2 > $tmp-out && ERROR_EXIT "TEST5-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST5-2"
+echo test5 OK
+
+#########
+# test6 #
+#########
+echo "自然数を指定してください" > $tmp-ans
+./gcf.sh -10 2 > $tmp-out && ERROR_EXIT "TEST6-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST6-2"
+echo test6 OK
+
+rm -f $tmp-*
+exit 0
+
